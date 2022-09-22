4e4fe36b7b6e976bd20764ac59f526f061fbd28f
RubenU2002 <rsantiagourrego@gmail.com>
2022-09-22 16:40:50 -0500

    Menu full y hover

--- javascript/head.js
+++ javascript/head.js
@@ -1,6 +1,7 @@
 const head__button = document.querySelector("#head__button");
 
 head__button.addEventListener('click',(e)=>{
+
     const linea1 = document.querySelector("#head__line1");
     const linea2 = document.querySelector("#head__line2");
     const desplegable = document.querySelector("#desplegable");
@@ -10,15 +11,22 @@ head__button.addEventListener('click',(e)=>{
     head__button.classList.toggle("borde");
     linea1.classList.toggle("head_click");
     linea2.classList.toggle("head_click2");
-    
+
+    const lista = document.querySelector("#desplegable");
+    const nodosClaseDesplegable = document.querySelectorAll(".sub_desplegable");
+    nodosClaseDesplegable.forEach(e =>{
+        e.classList.add("desaparecer");
+    });
+    lista.addEventListener('click',(e)=>{
+        if(e.target.parentNode.id =="peliculas" || e.target.parentNode.id =="disney+" || e.target.parentNode.id =="viajes" || e.target.parentNode.id =="tv" || e.target.parentNode.id =="mas"){
+            nodosClaseDesplegable.forEach(e1 =>{
+                if(e1!=e.target.parentNode.nextSibling.nextSibling){
+                    e1.classList.add("desaparecer");
+                }
+            });
+            e.preventDefault(); 
+            e.target.parentNode.nextSibling.nextSibling.classList.toggle("desaparecer");
+        }
+    });
 });
 
-const lista = document.querySelector("#desplegable");
-
-lista.addEventListener('click',(e)=>{
-    if(e.target.parentNode.id =="peliculas" || e.target.parentNode.id =="disney+" || e.target.parentNode.id =="viajes" || e.target.parentNode.id =="tv" || e.target.parentNode.id =="mas"){
-        e.preventDefault(); 
-        e.target.parentNode.nextSibling.nextSibling.classList.toggle("desaparecer");
-    }
-    
-});
